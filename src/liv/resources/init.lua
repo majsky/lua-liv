dbc309ccec78d716b88b90fbe0384daddffd9acc
Mário Mesíček <mario.mesicek@livelektra.sk>
2021-01-21 17:58:11 +0100

    patch caching

--- src/liv/resources/init.lua
+++ src/liv/resources/init.lua
@@ -1,9 +1,20 @@
 local d = {}
 
+local _TOKEN = "@FILENAME@"
+local cache = {}
+
 function d.getPath(f)
-  local tf = package.searchpath("liv.resources", package.path)
-  tf = tf:gsub("init.lua", f)
-  return tf
+  if cache.user[f] then
+    return cache.user[f]
+  end
+
+  if not cache.base then
+    local tf = package.searchpath("liv.resources", package.path)
+    cache.base = tf:gsub("init.lua", _TOKEN)
+  end
+
+  cache.user[f] = cache.base:gsub(_TOKEN, f)
+  return cache.user[f]
 end
 
 return d
