771e4ac17956b9a4f663f7437505ec608bb101c0
Mário Mesíček <mario.mesicek@livelektra.sk>
2021-01-27 16:03:31 +0100

    debugloader osamostatnenie

--- src/main.lua
+++ src/main.lua
@@ -12,11 +12,6 @@ if os.getenv("LOCAL_LUA_DEBUGGER_VSCODE") == "1" then
   require("lldebugger").start()
 end
 ]]
-print(lfs.currentdir ())
-package.cpath = package.cpath .. ";c:/Users/oresany/.vscode/extensions/tangzx.emmylua-0.3.49/debugger/emmy/windows/x86/?.dll"
-local dbg = require("emmy_core")
-dbg.tcpListen("localhost", 9966)
-dbg.waitIDE()
 
 ui.init(ui.modes.text)
 
