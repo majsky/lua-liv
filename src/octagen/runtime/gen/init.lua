912402853acbcb7666f0f393c4eda355a75f3b6f
Mário Mesíček <mario.mesicek@livelektra.sk>
2021-02-01 12:36:16 +0100

    pridany generator zoznamu pristrojov

--- src/octagen/runtime/gen/init.lua
+++ src/octagen/runtime/gen/init.lua
@@ -1,4 +1,21 @@
-local gens = {}
+local gens = {
+  pristroje = {}
+}
+
+function gens.pristroje.over(data)
+  return data.zozPrist ~= nil
+end
+function gens.pristroje.generuj(data)
+  local pristroje = {}
+
+  for k in pairs(data.zozPrist.db) do
+    table.insert(pristroje, k)
+  end
+
+  table.sort(pristroje)
+
+  return pristroje
+end
 
 local function registruj(generator)
   gens[generator] = require("octagen.runtime.gen." .. generator)
