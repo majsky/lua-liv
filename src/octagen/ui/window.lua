0c1e4568e58e25290ddad7ef96eb6c5b5bd460d0
Mário Mesíček <mario.mesicek@livelektra.sk>
2021-02-03 18:02:11 +0100

    Podpora "blbych" konzol

--- src/octagen/ui/window.lua
+++ src/octagen/ui/window.lua
@@ -25,31 +25,59 @@ function win.proto:settitle(title)
   return self
 end
 
+local function bgcol(style)
+  return style:match("([a-z]+)bg")
+end
+
 function win.proto:clear()
   local ts = nil
   if self.title then
     local tits = self.title
     local tl = #tits
-    local padn = math.floor((self.area.width - tl) / 2)
-    local pad = string.rep(" ", padn)
+    local padn = math.floor((self.area.width - tl) / 2) - 3
+    local pad = string.rep("-", padn)
 
-    if 2 * padn + tl ~= self.area.width then
-      tits = tits .. " "
-    end
+    local oneoff = 2 * padn + tl + 6 ~= self.area.width
 
-    ts = clr(colors.header, pad, tits, pad)
+    local titcol = bgcol(colors.header)
+    local titstring = {
+      clr(titcol .. "bg " .. titcol, "+" .. pad .. "[ "),
+      clr(colors.header, tits),
+      clr(titcol .. "bg " .. titcol, " ]" .. (oneoff and "-" or "") .. pad .. "+"),
+    }
+    ts = table.concat(titstring)
   else
-    ts = clr(colors.header, string.rep(" ", self.area.width))
+    local titcol = bgcol(colors.header)
+    local titstyle = titcol .. "bg " .. titcol
+    ts = table.concat({
+      clr(titstyle, "+"),
+      clr(titstyle, string.rep("-", self.area.width - 2)),
+      clr(titstyle, "+")
+    })
   end
 
   self.area:curpos(1,1)
   io.stdout:write(ts)
 
-  local empty = clr(colors.body, string.rep(" ", self.area.width))
-  for y = 2, self.area.height do
+  local bodyclr = bgcol(colors.body)
+
+  local empty = table.concat({
+    clr(bodyclr .. "bg " .. bodyclr, "|"),
+    clr(colors.body, string.rep(" ", self.area.width - 2)),
+    clr(bodyclr .. "bg " .. bodyclr, "|"),
+  })
+
+  for y = 2, self.area.height - 1 do
     self.area:curpos(1,y)
     io.stdout:write(empty)
   end
+
+  self.area:curpos(1, self.area.height)
+  io.stdout:write(
+    clr(bodyclr .. "bg " .. bodyclr, "+"),
+    clr(bodyclr .. "bg " .. bodyclr, string.rep("-", self.area.width - 2)),
+    clr(bodyclr .. "bg " .. bodyclr, "+")
+  )
 
   self:curpos(1,1)
   return self
